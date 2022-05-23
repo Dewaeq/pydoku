420605dfb8aebd687016ef64f52ca510c8ea2a03
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2022-05-23 22:10:45 +0200

    shrink large images

--- api.py
+++ api.py
@@ -19,11 +19,21 @@ def main():
 @app.post("/solve")
 def solve(file: UploadFile = File(...)):
     img = Image.open(file.file)
+    max_size = 1000
+
+    if img.width > max_size:
+        s = img.width / max_size
+        img = img.resize((int(img.width / s), int(img.height / s)))
+    elif img.height > max_size:
+        s = img.height / max_size
+        img = img.resize((int(img.width / s), int(img.height / s)))
+
     cv2_img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
     result = process(cv2_img)
 
+
     if result is None:
-        return HTMLResponse(content="h3>Failed to read sudoku</h3>")
+        return HTMLResponse(content="<h3>Failed to read sudoku</h3>")
 
     _, png_result = cv2.imencode(".png", result)
 
